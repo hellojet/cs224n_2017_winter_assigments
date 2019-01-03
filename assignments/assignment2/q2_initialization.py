22fd947708654949881edb1c13f86c7f1d161c74
Jieting Li <1130047048@qq.com>
2019-01-03 23:02:28 +0800

    xavier weight init

--- assignments/assignment2/q2_initialization.py
+++ assignments/assignment2/q2_initialization.py
@@ -25,6 +25,8 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
+        epsilon = tf.sqrt(6 / (shape[0] + shape[1]))
+        out = tf.random_uniform(shape, -epsilon, epsilon, dtype=tf.float32)
         ### END YOUR CODE
         return out
     # Returns defined initializer function.
